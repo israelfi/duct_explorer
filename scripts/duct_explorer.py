50d148987ba9775a96ba4c8b82cb6cf1044a1455
israelfi <israelfilipe.sa@gmail.com>
2022-06-15 19:54:34 -0300

    refactoring duct_explorer.py

--- scripts/duct_explorer.py
+++ scripts/duct_explorer.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 import numpy as np
+from datetime import datetime
+from math import sqrt, cos, sin, atan, pi, atan2
+
 import rospy
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from tf2_msgs.msg import TFMessage
 
-from math import sqrt, cos, sin, atan, pi, atan2
-
 
 class DuctExplorer:
     def __init__(self):
-        rospy.init_node('duct_explorer', anonymous=True)
-        self.freq = 10
-        self.rate = rospy.Rate(self.freq)
+        self.message_log('Starting duct_explorer node...')
+
+        self.__set_parameters()
 
         # Data used by this node
         self.robot_pos = np.zeros(3)
-        self.robot_angles = np.zeros(3)  # Euler angles
+        self.robot_angles = np.zeros(3)
         self.laser = {'ranges': np.array([]),
                       'angles': np.array([]),
                       'angle_min': 0.0,
                       'angle_max': 0.0,
                       'angle_increment': 0.0}
-        self.virtual_laser = {'ranges': [], 'angles': [], 'angle_min': 0.0, 'angle_max': 0.0, 'angle_increment': 0.0}
-
-        # Topics that this node interacts with
-        self.pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
-        rospy.Subscriber("/tf", TFMessage, self.callback_pose)
-        rospy.Subscriber("/scan", LaserScan, self.callback_laser)
+        self.virtual_laser = {'ranges': np.array([]),
+                              'angles': np.array([]),
+                              'angle_min': 0.0,
+                              'angle_max': 0.0,
+                              'angle_increment': 0.0}
 
         self.vel = Twist()
         self.vel.linear.x = 0
@@ -42,30 +42,91 @@ class DuctExplorer:
         self.started_pose = False
         self.started_laser = False
 
+        self.__init_node()
+
         self.rate.sleep()
 
-    def callback_laser(self, data):
+    def print_parameters(self) -> None:
+        """
+        Print the parameters of the node
+        Returns: None
+        """
+        print()
+        self.message_log('----------------- PARAMETERS -----------------')
+        self.message_log(f'Feedback linearization distance (m): {self.d}')
+        self.message_log(f'Convergence gain: {self.kf}')
+        self.message_log(f'Linear speed reference: {self.vr}')
+        self.message_log(f'Distance from wall (m): {self.epsilon}')
+        self.message_log('-----------------------------------------------\n')
+
+    def __set_parameters(self) -> None:
+        """
+        Set the parameters of the node
+        Returns: None
+        """
+        # Distance used in feedback linearization
+        self.d = 0.15
+        # Convergence gain
+        self.kf = 1
+        # Linear speed reference
+        self.vr = 0.25
+        # simulation
+        self.epsilon = 0.6
+        # CORO corridor
+        # epsilon = 1.25
+        self.print_parameters()
+
+    def __init_node(self) -> None:
+        """
+        Initialize the node subscribing to the needed topics
+        Returns:
+
+        """
+        rospy.init_node('duct_explorer', anonymous=True)
+        self.freq = 10
+        self.rate = rospy.Rate(self.freq)
+
+        # Topics that this node interacts with
+        self.pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+        rospy.Subscriber("/tf", TFMessage, self.callback_pose)
+        rospy.Subscriber("/scan", LaserScan, self.callback_laser)
+
+    @staticmethod
+    def message_log(msg: str, end='\n') -> None:
+        """
+        Prints a message to the console with timestamp.
+        Args:
+            msg: message to be printed
+            end: print function argument
+
+        Returns: None
+        """
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print(f"[{dt_string}] {msg}", end=end)
+
+    def callback_laser(self, data: LaserScan) -> None:
         """
         Callback routine to get the data from the laser sensor
         Args:
             data: laser data
         """
-        self.laser['ranges'] = np.array(data.ranges)
-        self.laser['angle_min'] = data.angle_min
-        self.laser['angle_max'] = data.angle_max
-        self.laser['angle_increment'] = data.angle_increment
+        laser = {'ranges': np.array(data.ranges),
+                 'angles': np.array([]),
+                 'angle_min': data.angle_min,
+                 'angle_max': data.angle_max,
+                 'angle_increment': data.angle_increment}
 
-        number_of_beams = int((self.laser['angle_max'] - self.laser['angle_min']) / self.laser['angle_increment'])
-
-        angle = self.laser['angle_min']
-        self.laser['angles'] = np.zeros(self.laser['ranges'].shape[0])
-        for i in range(self.laser['ranges'].shape[0]):
-            self.laser['angles'][i] = angle
-            angle += self.laser['angle_increment']
+        angle = laser['angle_min']
+        laser['angles'] = np.zeros(laser['ranges'].shape[0])
+        for i in range(laser['ranges'].shape[0]):
+            laser['angles'][i] = angle
+            angle += laser['angle_increment']
+        self.laser = laser
         self.create_virtual_laser()
         self.started_laser = True
 
-    def callback_pose(self, data):
+    def callback_pose(self, data: TFMessage) -> None:
         """
         Callback routine to get the pose information of the robot
         Args:
@@ -89,19 +150,20 @@ class DuctExplorer:
                 self.robot_pos[2] = T.transform.translation.z
         self.started_pose = True
 
-    def create_virtual_laser(self, dist=0.2):
+    def create_virtual_laser(self, dist: float = 0.2) -> None:
         """
-        Simulates a laser in front of real laser sensor
+        Simulates a laser in front of real laser sensor. The purpuse of this virtual laser is to smoothen the angular
+        displacement.
         Args:
             dist: how far the virtual laser will be in front of the current position
-        Returns:
+        Returns: None
         """
-        self.virtual_laser['angle_min'] = self.laser['angle_min']
-        self.virtual_laser['angle_max'] = self.laser['angle_max']
-        self.virtual_laser['angle_increment'] = self.laser['angle_increment']
+        virtual_laser = {'ranges': np.zeros(self.laser['ranges'].shape[0]),
+                         'angles': np.zeros(self.laser['ranges'].shape[0]),
+                         'angle_min': self.laser['angle_min'],
+                         'angle_max': self.laser['angle_max'],
+                         'angle_increment': self.laser['angle_increment']}
 
-        self.virtual_laser['ranges'] = []
-        self.virtual_laser['angles'] = []
         for i in range(self.laser['ranges'].shape[0]):
             x = self.laser['ranges'][i] * np.cos(self.laser['angles'][i])
             y = self.laser['ranges'][i] * np.sin(self.laser['angles'][i])
@@ -110,32 +172,38 @@ class DuctExplorer:
 
             d = np.sqrt(x ** 2 + y ** 2)
             th = atan2(y, x)
-            self.virtual_laser['ranges'].append(d)
-            self.virtual_laser['angles'].append(th)
+            virtual_laser['ranges'][i] = d
+            virtual_laser['angles'][i] = th
+
         new_data = []
-        for i in range(len(self.virtual_laser['ranges'])):
-            new_data.append((self.virtual_laser['ranges'][i], self.virtual_laser['angles'][i]))
+        for i in range(virtual_laser['ranges'].shape[0]):
+            new_data.append((virtual_laser['ranges'][i], virtual_laser['angles'][i]))
 
         # Ordering using angle
         new_data.sort(key=lambda k: k[1])
 
-        for i in range(len(self.virtual_laser['ranges'])):
-            self.virtual_laser['ranges'][i] = new_data[i][0]
-            self.virtual_laser['angles'][i] = new_data[i][1]
-        self.virtual_laser['ranges'] = np.array(self.virtual_laser['ranges'])
-        self.virtual_laser['angles'] = np.array(self.virtual_laser['angles'])
+        for i in range(len(virtual_laser['ranges'])):
+            virtual_laser['ranges'][i] = new_data[i][0]
+            virtual_laser['angles'][i] = new_data[i][1]
 
-    def closest_obstacle(self, right_side=True, half=None, virtual_laser=False):
+        virtual_laser['ranges'] = np.array(virtual_laser['ranges'])
+        virtual_laser['angles'] = np.array(virtual_laser['angles'])
+
+        self.virtual_laser = virtual_laser
+
+    def closest_obstacle(self, right_side: bool = True, half: int = None, virtual_laser: bool = False) -> tuple:
         """
         Returns the closest obstacle to the robot using a laser sensor.
         Args:
             right_side: a boolean indicating it is desired to know the closest obstacle on the right side (True) or on
             the left side (False)
             half: an integer informing where to section the right and left side
-            virtual_laser: bool to indicate if must use a virtual laser
+            virtual_laser: bool to indicate if virtual laser must be used
 
-        Returns: a tuple with the minimum distance of the closest obstacle, the angle where this distance was measured
-        and the index of this measurement in the laser vector
+        Returns: a tuple with:
+            - minimum distance of the closest obstacle
+            - angle where this distance was measured
+            - index of this measurement in the laser vector
         """
         if virtual_laser:
             laser_data = self.virtual_laser
@@ -154,15 +222,11 @@ class DuctExplorer:
             angle_of_closest_obstacle = laser_data['angles'][half:][index]
         return min_dist, angle_of_closest_obstacle, index
 
-    def follow_corridor(self):
+    def follow_corridor(self) -> tuple:
         """
         Control method to follow a corridor in the center of it
-        Returns: linear and angular velocities in the robot frame
+        Returns: a tuple with linear and angular velocities in the robot frame
         """
-        d = 0.15  # distance used in feedback linearization
-        kf = 1  # convergence gain
-        vr = 0.25  # linear velocity reference
-
         dist_r, phi_r, index_r = self.closest_obstacle(right_side=True, half=self.half)
         dist_l, phi_l, index_l = self.closest_obstacle(right_side=False, half=self.half)
 
@@ -173,50 +237,72 @@ class DuctExplorer:
 
         D = (dist_l - dist_r) / (2 * cos(alpha))
 
-        G = -(2 / pi) * atan(kf * D)
+        G = -(2 / pi) * atan(self.kf * D)
         H = sqrt(1 - G * G)
 
         vx = G * cos(phi_D) + H * cos(phi_T)  # (body)
         vy = G * sin(phi_D) + H * sin(phi_T)  # (body)
 
-        v = vr * vx
-        omega = vr * (vy / (d * 0.5))  # Angular rotation
+        v = self.vr * vx
+        omega = self.vr * (vy / (self.d * 0.5))  # Angular rotation
 
         return v, omega
 
-    def follow_wall(self):
+    def follow_wall(self) -> tuple:
         """
-        Control method to follow a wall
-        Returns: linear and angular velocities in the robot frame
+        Implementation of a vector field based control that makes the robot follow a tangent path alongside a wall
+
+        Returns: atuple with linear and angular velocities in the robot frame
         """
-        d = 0.15  # distance used in feedback linearization
-        kf = 1  # convergence gain
-        vr = 0.25  # linear velocity reference
-        # simulation
-        epsilon = 0.6
-        # CORO corridor
-        # epsilon = 1.25
 
         delta_m, phi_m, index_r = self.closest_obstacle(right_side=False, virtual_laser=True)
 
-        G = (2 / pi) * atan(kf * (delta_m - epsilon))
+        self.message_log(f'Closest obstacle distance (m): {round(delta_m, 2)} | '
+                         f'Angle (deg): {round(np.rad2deg(phi_m), 2)}')
+
+        G = (2 / pi) * atan(self.kf * (delta_m - self.epsilon))
         H = - sqrt(1 - G * G)  # + right side to the wall; - left side to the wall
 
-        v = vr * (cos(phi_m) * G - sin(phi_m) * H)
-        omega = vr * (sin(phi_m) * G / d + cos(phi_m) * H / d)
+        v = self.vr * (cos(phi_m) * G - sin(phi_m) * H)
+        omega = self.vr * (sin(phi_m) * G / self.d + cos(phi_m) * H / self.d)
 
         return v, omega
 
-    def main_service(self):
+    def wait_for_subscribed_topics(self) -> None:
+        """
+        Waits for the topics that are subscribed to by this topic.
+        Returns: None
+        """
+        self.message_log('Waiting for messages in subscribed topics...')
         while not self.started_pose or not self.started_laser:
             continue
+        self.message_log('Topics ready!')
+
+    def publish_speeds(self, linear: float, angular: float) -> None:
+        """
+        Publishes the linear and angular speeds to the robot.
+        Args:
+            linear: linear speed in the robot frame
+            angular: angular speed in the robot frame
+
+        Returns: None
+        """
+        self.message_log(f'Linear speed: {round(linear, 3)} | Angular Speed: {round(angular, 3)}')
+        self.vel.linear.x = linear
+        self.vel.angular.z = angular
+        self.pub_cmd_vel.publish(self.vel)
+
+    def main_service(self) -> None:
+        """
+        Main service method.
+
+        Returns: None
+        """
+        self.wait_for_subscribed_topics()
         while not rospy.is_shutdown():
             # v, w = self.follow_corridor()
             v, w = self.follow_wall()
-            self.vel.linear.x = v
-            self.vel.angular.z = w
-
-            self.pub_cmd_vel.publish(self.vel)
+            self.publish_speeds(linear=v, angular=w)
             self.rate.sleep()
 
 
